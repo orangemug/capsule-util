f364839f9b7b7350a1820d29733966033b1fcb71
orangemug <info@orangemug.co.uk>
2015-09-18 14:53:55 +0100

    Added in label filtering.

--- bin/_history.js
+++ bin/_history.js
@@ -3,10 +3,16 @@ var history = require("./../lib/history");
 
 module.exports = function(yargs) {
   var argv = yargs
+    .describe('labels', 'Pull out particular labeled points, noted with square brackets [like this]')
+    .array("labels")
+    .alias("l", "labels")
     .argv;
 
   var str = argv._[1];
-  history(str, function(err, html) {
+  history({
+    q: str,
+    labels: argv.labels
+  }, function(err, html) {
     if(err) {
       console.error(err);
       process.exit(1);
