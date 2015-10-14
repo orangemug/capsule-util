f2e87ebb6eeb2eac8d36893c4ac9c9182d3e4b20
orangemug <info@orangemug.co.uk>
2015-10-14 16:06:18 +0100

    Added page-breaks option.

--- bin/_history.js
+++ bin/_history.js
@@ -5,13 +5,15 @@ module.exports = function(yargs) {
   var argv = yargs
     .describe('labels', 'Pull out particular labeled points, noted with square brackets [like this]')
     .array("labels")
+    .describe('no-page-breaks', "Don't add page breaks")
     .alias("l", "labels")
     .argv;
 
   var str = argv._[1];
   history({
     q: str,
-    labels: argv.labels
+    labels: argv.labels,
+    pageBreaks: !!argv["no-page-breaks"]
   }, function(err, html) {
     if(err) {
       console.error(err);
